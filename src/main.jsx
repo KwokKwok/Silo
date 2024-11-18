1806eb7db1831330bcaf67d9e394eb72ddd01aab
郭垒 <kwokglory@outlook.com>
2024-11-18 21:14:51 +0800

    feat: vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import './i18n';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 );
