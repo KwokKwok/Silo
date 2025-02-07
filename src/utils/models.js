72543b304d7f78e5cbbfee6c181996e76cf30e89
郭垒 <kwokglory@outlook.com>
2025-02-07 13:20:49 +0800

    feat: add deepseek pro models

--- src/utils/models.js
+++ src/utils/models.js
@@ -144,6 +144,8 @@ const SILICON_MODELS = [
   // textModelOf("meta-llama/Meta-Llama-3-8B-Instruct", 0, 8, true),
   textModelOf("deepseek-ai/DeepSeek-R1", 16, 64, false, false),
   textModelOf("deepseek-ai/DeepSeek-V3", 8, 64, false, false),
+  textModelOf("Pro/deepseek-ai/DeepSeek-R1", 16, 64, false, false),
+  textModelOf("Pro/deepseek-ai/DeepSeek-V3", 8, 64, false, false),
   textModelOf("Qwen/QwQ-32B-Preview", 1.26, 32, false, false),
   textModelOf("Qwen/QVQ-72B-Preview", 9.9, 32, false, true),
   // textModelOf("Tencent/Hunyuan-A52B-Instruct", 21, 32, false),
